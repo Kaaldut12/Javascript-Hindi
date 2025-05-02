8feeab066cfeb480504e5acfe910020f9accf34e
Abhay Patel <96361907+Kaaldut12@users.noreply.github.com>
2025-05-02 06:12:09 +0000

    variable

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+const accountId= 12345
+let accountEmail = "abhay@gmail.com"
+var accountPassword = "abhay@123"
+accountCity = "Varanasi"
+
+// accountId = 54321
+accountEmail = "gmail.abhay@com"
+accountPassword = "123abhay@"
+accountCity="Harsos"
+let accountState;
+
+console.table([accountId,accountEmail, accountPassword, accountCity,accountState])
